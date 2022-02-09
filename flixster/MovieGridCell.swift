e09d73be8aac17588c9f1dcb846abb5246998382
mishanguyen <nguyen14@usf.edu>
2022-02-08 23:13:58 -0500

    Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Misha Nguyen on 2/8/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
